002b56a07ab9d2b81b51908ce82a0ef6a3369b08
Shockk <shokku.ra@gmail.com>
2016-04-01 04:56:57 +0000

    Fix compilation with clang and llvm
    Linker ordering is required :(

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <boost/make_shared.hpp>
+#include <boost/container/vector.hpp>
 #include <boost/thread.hpp>
 #include <boost/asio.hpp>
-#include <clang/Basic/DiagnosticOptions.h>
+//#include <clang/CodeGen/CodeGenAction.h>
+//#include <clang/Basic/DiagnosticOptions.h>
+//#include <clang/Driver/Compilation.h>
+//#include <clang/Driver/Driver.h>
+//#include <clang/Driver/Tool.h>
+//#include <clang/Frontend/CompilerInstance.h>
+//#include <clang/Frontend/CompilerInvocation.h>
+//#include <clang/Frontend/FrontendDiagnostic.h>
+#include <clang/Frontend/TextDiagnosticPrinter.h>
+//#include <llvm/ADT/SmallString.h>
+//#include <llvm/ExecutionEngine/ExecutionEngine.h>
+//#include <llvm/ExecutionEngine/MCJIT.h>
+//#include <llvm/IR/Module.h>
+//#include <llvm/Support/FileSystem.h>
+//#include <llvm/Support/Host.h>
+//#include <llvm/Support/ManagedStatic.h>
+//#include <llvm/Support/Path.h>
+//#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
 #include "ibotpp.hpp"
 #include "config.h"
 
@@ -10,7 +29,12 @@ void worker_main(boost::shared_ptr<boost::asio::io_service> io_service) {
 }
 
 int main(int argc, char *argv[]) {
-	clang::IntrusiveRefCntPtr<clang::DiagnosticOptions> diag_opts(new clang::DiagnosticOptions());
+	boost::container::vector<const char *> args;
+	args.emplace_back("test.cpp");
+	llvm::IntrusiveRefCntPtr<clang::DiagnosticOptions> diag_opts(new clang::DiagnosticOptions());
+	auto diag_printer = boost::make_shared<clang::TextDiagnosticPrinter>(llvm::errs(), diag_opts.get());
+	llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs> diag_ids(new clang::DiagnosticIDs());
+	clang::DiagnosticsEngine diag_engine(diag_ids, diag_opts.get(), diag_printer.get());
 
 	auto io_service = boost::make_shared<boost::asio::io_service>();
 	auto work = boost::make_shared<boost::asio::io_service::work>(*io_service);
