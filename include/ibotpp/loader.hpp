42fe256b66696c0f92e5bddc63bf61a31be7065b
Shockk <shokku.ra@gmail.com>
2016-04-04 19:16:48 +0000

    loader: set native target invocation flags using driver

--- include/ibotpp/loader.hpp
+++ include/ibotpp/loader.hpp
@@ -5,10 +5,14 @@
 #include <string>
 #include <boost/container/vector.hpp>
 #include <clang/CodeGen/CodeGenAction.h>
+#include <clang/Driver/Compilation.h>
+#include <clang/Driver/Driver.h>
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/CompilerInvocation.h>
 #include <clang/Frontend/TextDiagnosticPrinter.h>
 #include <llvm/IR/Module.h>
+#include <llvm/Support/Host.h>
+#include <llvm/Support/Program.h>
 #include <llvm/Support/raw_ostream.h>
 
 namespace ibotpp {
@@ -17,6 +21,7 @@ namespace ibotpp {
 		llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs> diag_ids;
 		llvm::IntrusiveRefCntPtr<clang::DiagnosticOptions> diag_options;
 		clang::DiagnosticsEngine diag_engine;
+		std::unique_ptr<clang::driver::Driver> driver;
 		llvm::IntrusiveRefCntPtr<clang::CompilerInvocation> invocation;
 		clang::CompilerInstance compiler;
 
@@ -25,11 +30,21 @@ namespace ibotpp {
 				: diag_ids(new clang::DiagnosticIDs()),
 				  diag_options(new clang::DiagnosticOptions()),
 				  diag_engine(diag_ids, diag_options.get(),
-					new clang::TextDiagnosticPrinter(llvm::errs(), diag_options.get())),
+					new clang::TextDiagnosticPrinter(llvm::errs(),
+					                                 diag_options.get())),
 				  invocation(new clang::CompilerInvocation()) {
+			auto error_or_clang_path = llvm::sys::findProgramByName("clang");
+			if(!error_or_clang_path) {
+				throw std::runtime_error("failed to find path of clang executable");
+			}
+			driver = std::unique_ptr<clang::driver::Driver>(
+				new clang::driver::Driver(error_or_clang_path.get(),
+			                              llvm::sys::getDefaultTargetTriple(),
+			                              diag_engine));
+			driver->setCheckInputsExist(false);
 			compiler.createDiagnostics();
 			if(!compiler.hasDiagnostics()) {
-				throw std::runtime_error("compiler instance does not have diagnostics");
+				throw std::runtime_error("failed to create compiler instance with diagnostics");
 			}
 		}
 
@@ -38,11 +53,16 @@ namespace ibotpp {
 		}
 
 		std::unique_ptr<llvm::Module> load_llvm_module(const std::string &path) {
-			boost::container::vector<const char *> args;
-			args.emplace_back("-std=c++11");
-			args.emplace_back("-include");
-			args.emplace_back("module.hpp");
-			args.emplace_back(path.c_str());
+			auto compilation = driver->BuildCompilation({"-std=c++11",
+			                                             "-fsanitize=address",
+			                                             "-include", "module.hpp",
+			                                             path.c_str()});
+			if(!compilation) {
+				throw std::runtime_error("failed to build driver compilation");
+			}
+			auto &jobs = compilation->getJobs();
+			auto &cmd = llvm::cast<clang::driver::Command>(*jobs.begin());
+			auto &args = cmd.getArguments();
 			clang::CompilerInvocation::CreateFromArgs(*invocation, args.data(),
 			                                          args.data() + args.size(),
 			                                          diag_engine);
